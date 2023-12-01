29f46a0d3df8a7aa9ead4b2c0a2ccdd572694ec7
Agbode-Milan <agbodekacalebkm@gmail.com>
2023-11-30 17:59:56 -0800

    a Bash script that sends a request to a URL passed as an argument, and displays only the status code of the response

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# send a POST request to the passed URL using curl, and display the body of the response
-curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+# sends a request to a URL, and displays only the status code of the response.
+curl -s -o /dev/null -w "%{http_code}" "$1"
